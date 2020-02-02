6a251e8da48f9aa0fdf8bee52bf3b05db2df77d7
Shayan <naghizadehsh@gmail.com>
2020-02-02 14:27:15 -0500

    Added a new method for the Employee class

--- oop1.py
+++ oop1.py
@@ -8,24 +8,18 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = first + "." + last + "@dreamber.com"
+    
+    def full_name(self):
+        print("{} {}".format(self.first, self.last))
 
-emp_1 = Employee(Sam, Morin, 50_000)
-emp_2 = Employee(Helen, Shiri, 50_000)
+emp_1 = Employee("Sam", "Morin", 50_000)
+emp_2 = Employee("Helen", "Shiri", 50_000)
 
-print(emp_1)
-print(emp_2)
-
-emp_1.first = "Sam"
-emp_1.last = "Morin"
-emp_1.email = "sam.morin@dreamber.com"
-emp_1.pay = 50_000
-
-
-emp_2.first = "Test"
-emp_2.last = "Tester"
-emp_2.email = "test.tester@dreamber.com"
-emp_2.pay = 60_000
+#print(emp_1)
+#print(emp_2)
 
 print(emp_1.email)
 print(emp_2.email)
 
+print("{} {}".format(emp_1.first, emp_1.last))
+print("{} {}".format(emp_2.first, emp_2.last))
